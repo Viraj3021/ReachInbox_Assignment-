6c43090e117751f2a9c6e1048eb1a83355318bdb
VIRAJ BHOSALE <85681437+Viraj3021@users.noreply.github.com>
2024-08-11 08:47:27 -0500

    Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,7 +13,7 @@ function Login() {
 
   const handleGoogleLogin = () => {
     // Redirect to Google login URL
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://frontend.com";
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reachinbox-assignment-wine.vercel.app/login";
   };
 
   return (
